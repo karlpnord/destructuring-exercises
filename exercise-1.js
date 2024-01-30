85abb9148469b7b9c97c63212b4704f6807cc3ca
karlpn <91100704+karlpn@users.noreply.github.com>
2024-01-30 14:47:14 +0100

    excersises finished

--- exercise-1.js
+++ exercise-1.js
@@ -2,8 +2,22 @@
 // object below and then log all of them
 
 const car = {
-  manufacturer: 'Nissan',
-  model: 'Skyline GT-R',
-  country: 'Japan',
-  coupe: true,
+   manufacturer: "Nissan",
+   model: "Skyline GT-R",
+   country: "Japan",
+   coupe: true,
+   test: {
+      hallo: "1",
+      goodbye: "2",
+   },
 };
+
+const {
+   manufacturer,
+   model,
+   country,
+   coupe,
+   test: { hallo, goodbye },
+} = car;
+
+console.log("Car:", manufacturer, model, country, coupe, hallo, goodbye);
--- exercise-2.js
+++ exercise-2.js
@@ -5,3 +5,7 @@
 // them correctly.
 
 const values = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+const [firstValue, secondValue, , , fifthValue, ...rest] = values;
+
+console.log(firstValue, secondValue, fifthValue, rest);
